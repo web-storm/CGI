1691e80f6852e05f84bd47f8803b484f7b723782
web-storm <s-t-o-r-m@list.ru>
2015-10-03 11:11:31 +0400

    ready to 2nd lab

--- Lab1/src/script/script.js
+++ Lab1/src/script/script.js
@@ -1,26 +1,32 @@
 function search() {
-    var singer = $('#singer').val();
+    var term = $('#term').val();
+    if (!term) {
+        $('#warning').show();
+        return;
+    }
     $.ajax({
-        url: 'script/itunes.json',
-        //url: 'https://itunes.apple.com/search',
+        //url: 'script/itunes.json',
+        url: 'https://itunes.apple.com/search',
         type: 'GET',
-        /*data: {
-            term: singer,
-            limit: 25
+        data: {
+            term: term
         },
         dataType: 'jsonp',
-        crossDomain: true,*/
+        crossDomain: true,
         success: displayData
     });
+    $('#warning').hide();
 }
 
 function displayData(data) {
-    console.log(data);
-    moment().format('MMM Do YYYY');
-    data.results.each(function(item) {
-        item.releaseDate = moment(item.releaseDate);
+    data.results.forEach(function(item) {
+        item.releaseDate = moment(item.releaseDate).format('MMM Do YYYY');
     });
-    $( "#dataList" ).html(
-        $( "#dataTemplate" ).render( data.results )
+    $( '#dataList' ).html(
+        $( '#dataTemplate' ).render( data.results )
     );
 }
+
+$(function() {
+   $('#submit_btn').click(search);
+});
