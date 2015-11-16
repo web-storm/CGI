51c55b34c5d6acf1a56e671361801174f1fd57bc
web-storm <s-t-o-r-m@list.ru>
2015-11-16 15:54:02 +0300

    lab2 - completed

--- Lab1/DevDir/bower_components/jsrender/index.js
+++
@@ -1,8 +0,0 @@
-/*! JsRender
- * Version for web: jsrender.js
- * Version for Node.js: jsrender-node.js
- */
-
-'use strict';
-
-module.exports = require('./jsrender-node.js');
--- Lab1/gulpfile.js
+++ Lab1/gulpfile.js
@@ -1,47 +1,131 @@
+//dependecies
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-//var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
 var del = require('del');
-var uglify = require('gulp-uglify');
-
-//Очистка папки для Dev
-gulp.task('clean_app', function() {
-    return del.sync(['./DevDir/**']);
+var filter = require('gulp-filter');
+var htmlminify = require('gulp-minify-html');
+var mainBowerFiles = require('main-bower-files');
+var $ = require("gulp-load-plugins")({
+    lazy: true,
+    pattern: [
+        "gulp-*",
+        "gulp.*",
+        "main-bower-files"
+    ]
 });
 
-// Дев
-// Копирование файлов в папку DevDir
-gulp.task('DevMustHaveComponents', function() {
-    gulp.src('./bower_components/**')
-        .pipe(gulp.dest('./DevDir/bower_components'));
+//paths
+var basePaths = {
+    src: 'src/',
+    dest: 'build/',
+    bower: 'bower_components/'
+};
+var paths = {
+    js: {
+        src: basePaths.src + 'js/',
+        devDest: basePaths.dest + 'development/js/',
+        prodDest: basePaths.dest + 'production/js/'
+    },
+    styles: {
+        src: basePaths.src + 'css/',
+        devDest: basePaths.dest + 'development/css/',
+        prodDest: basePaths.dest + 'production/css/'
+    },
+    html: {
+        src: basePaths.src,
+        devDest: basePaths.dest + 'development/',
+        prodDest: basePaths.dest + 'production/'
+    }
+};
+
+//development and production cleaning up
+gulp.task('build-dev-clean', function() {
+    return del.sync([paths.js.devDest, paths.styles.devDest, paths.html.devDest]);
+});
+gulp.task('build-prod-clean', function() {
+    return del.sync([paths.js.prodDest, paths.styles.prodDest, paths.html.prodDest]);
 });
 
-gulp.task('DevSrc', function() {
-    gulp.src(['./src/**','!./src/**/*.json'])
-        .pipe(gulp.dest('./DevDir'));
-    //todo: разделить стримы
+//development build
+gulp.task('build-bower-dev', function() {
+    var jsFilter = $.filter('*.js', {restore: true});
+    var cssFilter = $.filter('*.css', {restore: true});
+
+    return gulp.src(mainBowerFiles())
+        .pipe(jsFilter)
+        .pipe(gulp.dest(paths.js.devDest))
+        .pipe(jsFilter.restore)
+        .pipe(cssFilter)
+        .pipe(gulp.dest(paths.styles.devDest));
+});
+gulp.task('build-js-dev', function() {
+    return gulp.src(paths.js.src+'*.js')
+        .pipe($.jshint())// - улучшение js
+        .pipe(gulp.dest(paths.js.devDest));
+});
+gulp.task('build-css-dev', function() {
+    return gulp.src(paths.styles.src+'*.css')
+        .pipe(gulp.dest(paths.styles.devDest));
+});
+gulp.task('build-html-dev', function() {
+    return gulp.src(paths.html.src+'*.html')
+        .pipe(gulp.dest(paths.html.devDest));
+});
+gulp.task('build-dev', function() {
+    runSequence('build-dev-clean',
+        ['build-js-dev', 'build-css-dev', 'build-bower-dev'],
+        'build-html-dev');
 });
 
-
-// Продакшн
-// Конкатенация и минификация файлов
-gulp.task('prod', function(){
-    gulp.src('./src/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('./dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist'));
+//production build
+gulp.task('build-bower-prod', function() {
+    var jsFilter = $.filter('*.js', {restore: true});
+    var cssFilter = $.filter('*.css', {restore: true});
+    return gulp.src(mainBowerFiles())
+        .pipe(jsFilter)
+        .pipe($.concat('all.min.js'))
+        .pipe($.uglify())
+        .pipe(gulp.dest(paths.js.prodDest))
+        .pipe(jsFilter.restore)
+        .pipe(cssFilter)
+        .pipe($.uncss({ html: [paths.html.src+'index.html'] }))
+        .pipe($.csso())
+        .pipe($.rename('bootstrap.min.css'))
+        .pipe(gulp.dest(paths.styles.prodDest));
+});
+gulp.task('build-js-prod', function() {
+    return gulp.src(paths.js.src+'*.js')
+        .pipe($.jshint())// - улучшение js
+        .pipe($.uglify())
+        .pipe($.rename('script.min.js'))
+        .pipe(gulp.dest(paths.js.prodDest));
+});
+gulp.task('build-css-prod', function() {
+    return gulp.src(paths.styles.src+'*.css')
+        .pipe($.uncss({ html: [paths.html.src+'index.html'] }))
+        .pipe($.csso())
+        .pipe($.rename('style.min.css'))
+        .pipe(gulp.dest(paths.styles.prodDest));
+});
+gulp.task('build-html-prod', function() {
+    return gulp.src(paths.html.src+'*.html')
+        .pipe($.processhtml({}))
+        .pipe(htmlminify({}))
+        .pipe(gulp.dest(paths.html.prodDest));
+});
+gulp.task('build-prod', function() {
+    runSequence('build-prod-clean',
+    ['build-js-prod', 'build-css-prod', 'build-bower-prod',
+    'build-html-prod']);
 });
 
-// Действия по умолчанию
-gulp.task('default', function(){
-    gulp.run('clean_app');
-    gulp.run('DevMustHaveComponents');
-    gulp.run('DevSrc');
-//gulp.task('default', ['clean_app', 'Dev']);
+//default actions
+gulp.task('default', function() {
+    runSequence('build-dev', 'build-prod');
+});
 
-    // Отслеживаем изменения в файлах
-   // gulp.watch("./src/*.js", function(event){
-    //    gulp.run('Dev');
+gulp.task('watch', function() {
+    gulp.watch(paths.styles.src + "*.css", ['build-css-dev']);
+    gulp.watch(paths.html.src + "*.html", ['build-html-dev']);
+    gulp.watch(basePaths.bower, ['build-bower-dev']);
 });
